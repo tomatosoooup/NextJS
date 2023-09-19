b8f75bf59ed4056c20b14bdee00e013c8ee5f9f6
Vasyl <v.gabinet02@icloud.com>
2023-09-19 14:17:48 +0300

    text animations added

--- currency-app/app/[locale]/components/sections/Services.tsx
+++ currency-app/app/[locale]/components/sections/Services.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useFonts } from "@/providers/FontProvider";
+import { useTranslations } from "next-intl";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useTranslations } from "next-intl";
-import Image from "next/image";
 import { useLayoutEffect, useRef } from "react";
 
+import Image from "next/image";
 import classes from "@/app/animations/Image.module.css";
 
 const images = [
@@ -50,25 +50,107 @@ const images = [
 const Services = () => {
   const fonts = useFonts();
   const t = useTranslations("Services");
-  const ul1 = useRef(null);
-  const ul2 = useRef(null);
 
-  useLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
+  const liList = [
+    {
+      content: t("l1"),
+      subcontent: (
+        <>
+          <span className="text-[#9F9F9F]">{t("l1-")}</span>
+          <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
+        </>
+      ),
+    },
+    {
+      content: t("l2"),
+      subcontent: (
+        <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
+      ),
+    },
+    {
+      content: t("l3"),
+      subcontent: (
+        <>
+          <span className="text-[#9F9F9F]">(UAH)</span>, PSP
+          <span>{t("l3-")}</span>
+          <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
+        </>
+      ),
+    },
+    {
+      content: t("l4"),
+      subcontent: (
+        <>
+          <span className="text-[#9F9F9F]">{t("l4-")}</span>
+          <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
+        </>
+      ),
+    },
+  ];
 
-    const timeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: ul1.current,
-        start: "top-=300px 80%",
-        end: "top-=30px 60%",
-        scrub: true,
-      },
-    });
+  const liListSecond = [
+    {
+      content:
+        "Широкий спектр юрисдикций и компаний для специализированого обслуживания клиентов",
+      subcontent: (
+        <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
+      ),
+    },
+    {
+      content: "Прием/выставление инвойсов и контрактов",
+      subcontent: (
+        <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
+      ),
+    },
+    {
+      content: "Пополнение физ.лиц",
+      subcontent: (
+        <>
+          <span className="text-[#9F9F9F]">
+            (оплаты квартир, обучений, переводы семье)
+          </span>
+          <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
+        </>
+      ),
+    },
+    {
+      content: "Оплата товаров и автомобильных аукционов по всему миру",
+      subcontent: (
+        <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
+      ),
+    },
+  ];
 
-    timeline
-      .from(ul1.current, { y: 200, x: -100, opacity: 0, rotate: 180 })
-      .from(ul2.current, { opacity: 0, scale: 0 }, 0);
-  }, []);
+  function AnimatedText({
+    children,
+    className,
+  }: {
+    children: React.ReactNode;
+    className: string;
+  }) {
+    const text = useRef(null);
+
+    useLayoutEffect(() => {
+      gsap.registerPlugin(ScrollTrigger);
+
+      gsap.from(text.current, {
+        scrollTrigger: {
+          trigger: text.current,
+          start: "0px bottom",
+          end: "bottom+=175px bottom",
+          scrub: true,
+        },
+        x: -400,
+        opacity: 0,
+      });
+    }, []);
+    return (
+      <li ref={text} className={className}>
+        {children}
+      </li>
+    );
+  }
+
   return (
     <>
       <section className="max-w-screen-2xl m-auto">
@@ -87,56 +169,22 @@ const Services = () => {
               <div className="my-5 lg:my-10 w-72 h-[1px] bg-gradient-to-l from-white to-white/10 md:hidden"></div>
               <div className="my-5 lg:my-10 w-72 h-[1px] bg-gradient-to-r from-white to-white/10"></div>
             </div>
-            <ul
-              className="flex flex-col max-w-xl gap-6 text-[#EAEAEA] text-lg font-normal relative mt-3"
-              ref={ul1}
-            >
-              <li className="relative pl-4">
-                {t("l1")}
-                <span className="text-[#9F9F9F]">{t("l1-")}</span>
-                <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
-              </li>
-              <li className="relative pl-4">
-                {t("l2")}
-                <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
-              </li>
-              <li className="relative pl-4">
-                {t("l3")}
-                <span className="text-[#9F9F9F]">(UAH)</span>, PSP
-                <span>{t("l3-")}</span>
-                <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
-              </li>
-              <li className="relative pl-4">
-                {t("l4")}
-                <span className="text-[#9F9F9F]">{t("l4-")}</span>
-                <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
-              </li>
+            <ul className="flex flex-col max-w-xl gap-6 text-[#EAEAEA] text-lg font-normal relative mt-3">
+              {liList.map((li) => (
+                <AnimatedText className="relative pl-4" key={li.content}>
+                  {li.content}
+                  {li.subcontent}
+                </AnimatedText>
+              ))}
             </ul>
             {/* Second ul */}
-            <ul
-              className="flex flex-col max-w-xl gap-6 text-[#EAEAEA] text-lg font-normal mt-5 md:mt-14 md:ml-[22%] relative"
-              ref={ul2}
-            >
-              <li className="relative pl-4">
-                Широкий спектр юрисдикций и компаний для специализированого
-                обслуживания клиентов
-                <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
-              </li>
-              <li className="relative pl-4">
-                Прием/выставление инвойсов и контрактов
-                <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
-              </li>
-              <li className="relative pl-4">
-                Пополнение физ.лиц
-                <span className="text-[#9F9F9F]">
-                  (оплаты квартир, обучений, переводы семье)
-                </span>
-                <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
-              </li>
-              <li className="relative pl-4">
-                Оплата товаров и автомобильных аукционов по всему миру
-                <div className="w-1 h-1 rounded-full bg-white absolute -left-0 top-3"></div>
-              </li>
+            <ul className="flex flex-col max-w-xl gap-6 text-[#EAEAEA] text-lg font-normal mt-5 md:mt-14 md:ml-[22%] relative">
+              {liListSecond.map((li) => (
+                <AnimatedText className="relative pl-4" key={li.content}>
+                  {li.content}
+                  {li.subcontent}
+                </AnimatedText>
+              ))}
             </ul>
           </div>
           {images.map((img) => (
