cf2639c60d57939be6aa7112ec1a6708dd15f8b0
Vasyl <v.gabinet02@icloud.com>
2023-10-15 16:05:42 +0300

    changed bank img/mb no lags now

--- currency-app/app/[locale]/components/sections/Main.tsx
+++ currency-app/app/[locale]/components/sections/Main.tsx
@@ -12,8 +12,8 @@ const images = [
   {
     alt: "Bank",
     src: "/images/Bank.png",
-    width: 600,
-    height: 415,
+    width: 650,
+    height: 625,
     class: "hidden lg:block pointer-events-none filter",
   },
   {
@@ -29,16 +29,17 @@ const Main = () => {
   const t = useTranslations("Main");
   const fonts = useFonts();
 
-  return <>
-    <div className="max-w-screen-2xl m-auto ">
-      <Numbers
-        containerClass="text-8xl top-0 left-20 main"
-        spanClass="blur-xl"
-        nums={[1, 0, 1, 0, 0, 1]}
-      />
-      <section>
-        <div
-          className="
+  return (
+    <>
+      <div className="max-w-screen-2xl m-auto ">
+        <Numbers
+          containerClass="text-8xl top-0 left-20 main"
+          spanClass="blur-xl"
+          nums={[1, 0, 1, 0, 0, 1]}
+        />
+        <section>
+          <div
+            className="
       lg:grid
       lg:grid-cols-2 
       text-white 
@@ -46,95 +47,97 @@ const Main = () => {
       md:mb-32 
       lg:mb-20 
       relative"
-        >
-          <div
-            className="px-5 
+          >
+            <div
+              className="px-5 
                       text-center 
                       lg:text-justify 
                       pt-32
                       relative"
-          >
-            <Numbers
-              containerClass="text-5xl top-0 right-64 pt-32 main"
-              spanClass="blur"
-              nums={[0, 1, 1, 0, 1]}
-            />
-            <Reveal options={{ opc: 0, x: -500, del: 0.25 }}>
-              <h1
-                className="text-3xl
+            >
+              <Numbers
+                containerClass="text-5xl top-0 right-64 pt-32 main"
+                spanClass="blur"
+                nums={[0, 1, 1, 0, 1]}
+              />
+              <Reveal options={{ opc: 0, x: -500, del: 0.25 }}>
+                <h1
+                  className="text-3xl
             md:text-6xl 
             uppercase 
             mb-0
             sm:mb-5 
             font-bold 
             relative"
-                style={{ fontFamily: `${fonts.ct}` }}
-              >
-                Cryptocurrency exchange
-              </h1>
-            </Reveal>
-            <Reveal options={{ opc: 0, x: -500, del: 0.5 }} width="100%">
-              <div className="flex justify-center lg:justify-start">
-                <div className="my-5 w-72 h-[1px] bg-gradient-to-l from-white to-white/10 lg:hidden"></div>
-                <div className="my-5 w-72 h-[1px] bg-gradient-to-r from-white to-white/10"></div>
-              </div>
-            </Reveal>
-            <Reveal options={{ opc: 0, x: -500, del: 0.5 }}>
-              <p
-                className="text-sm md:text-xl text-[#CECECE] font-light lg:max-w-xl"
-                style={{ fontFamily: `${fonts.tt}` }}
-              >
-                <span className="font-semibold">{t("p1")}</span>
-                <br />
-                <br />
-
-                <span className="font-light">{t("p2")}</span>
-              </p>
-            </Reveal>
-
-            <Reveal options={{ opc: 0, x: -500, del: 0.75 }}>
-              <div className="hidden lg:block mt-10">
-                <Button>
-                  <span className="pt-1">{t("button")}</span>
-                </Button>
-              </div>
-            </Reveal>
-          </div>
-          <div>
-            {images.map((img, index) => (
-              <Reveal options={{ opc: 0, x: 500, del: 0.75 }} key={index}>
-                <Image
-                  alt={img.alt}
-                  src={img.src}
-                  width={img.width}
-                  height={img.height}
-                  className={img.class}
-                  key={img.alt}
-                  loading="eager"
-                  sizes="100vw"
-                  style={{
-                    width: "100%",
-                    height: "auto"
-                  }} />
+                  style={{ fontFamily: `${fonts.ct}` }}
+                >
+                  Cryptocurrency exchange
+                </h1>
               </Reveal>
-            ))}
+              <Reveal options={{ opc: 0, x: -500, del: 0.5 }} width="100%">
+                <div className="flex justify-center lg:justify-start">
+                  <div className="my-5 w-72 h-[1px] bg-gradient-to-l from-white to-white/10 lg:hidden"></div>
+                  <div className="my-5 w-72 h-[1px] bg-gradient-to-r from-white to-white/10"></div>
+                </div>
+              </Reveal>
+              <Reveal options={{ opc: 0, x: -500, del: 0.5 }}>
+                <p
+                  className="text-sm md:text-xl text-[#CECECE] font-light lg:max-w-xl"
+                  style={{ fontFamily: `${fonts.tt}` }}
+                >
+                  <span className="font-semibold">{t("p1")}</span>
+                  <br />
+                  <br />
+
+                  <span className="font-light">{t("p2")}</span>
+                </p>
+              </Reveal>
+
+              <Reveal options={{ opc: 0, x: -500, del: 0.75 }}>
+                <div className="hidden lg:block mt-10">
+                  <Button>
+                    <span className="pt-1">{t("button")}</span>
+                  </Button>
+                </div>
+              </Reveal>
+            </div>
+            <div>
+              {images.map((img, index) => (
+                <Reveal
+                  options={{ opc: 0, x: 500, del: 0.75 }}
+                  key={index}
+                  width="100%"
+                >
+                  <Image
+                    alt={img.alt}
+                    src={img.src}
+                    width={img.width}
+                    height={img.height}
+                    className={img.class}
+                    key={img.alt}
+                    loading="eager"
+                  />
+                </Reveal>
+              ))}
+            </div>
           </div>
-        </div>
-        <Image
-          alt="Bitcoin"
-          src={"./images/Bitcoin.svg"}
-          width={375}
-          height={375}
-          className="absolute left-0 -bottom-36 hidden lg:block pointer-events-none"
-          loading="eager"
-          quality={100}
-          style={{
-            maxWidth: "100%",
-            height: "auto"
-          }} />
-      </section>
-    </div>
-  </>;
+          <Image
+            alt="Bitcoin"
+            src={"./images/Bitcoin.svg"}
+            width={375}
+            height={375}
+            className="absolute left-0 -bottom-36 hidden lg:block pointer-events-none"
+            loading="eager"
+            quality={100}
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+          />
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Main;
