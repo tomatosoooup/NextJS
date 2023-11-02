4fdc91c41ecab49a2c74d77fe1cea3f8b7ebc2cb
Vasyl <v.gabinet02@icloud.com>
2023-11-02 14:19:36 +0200

    changed logo

--- currency-app/app/[locale]/sections/FormSections/FormLogo.tsx
+++ currency-app/app/[locale]/sections/FormSections/FormLogo.tsx
@@ -6,18 +6,18 @@ const FormLogo = () => {
   return (
     <>
       <div
-        className="text-center font-medium text-white mx-auto mt-10 hidden lg:block lg:absolute lg:bottom-10 lg:left-[50%] lg:translate-x-[-50%] order-5 lg:order-none"
+        className="text-center font-medium text-white mx-auto hidden lg:block lg:absolute lg:bottom-10 lg:left-[50%] lg:translate-x-[-50%] mb-5"
         style={{ fontFamily: `${fonts.cf}` }}
       >
         <Reveal options={{ x: 0, bottom: 100 }}>
           <>
             <div className="flex flex-col relative">
               <div className="w-12 md:w-24 lg:w-56 h-[1px] bg-gradient-to-l from-white to-white/10 absolute -left-16 md:-left-32 lg:-left-64 top-11 lg:top-16"></div>
-              <span className="text-5xl lg:text-7xl  tracking-[0.7rem] font-extrabold">
+              <span className="lg:text-6xl tracking-[0.7rem] font-extrabold mb-2">
                 KLTP
               </span>
               <span
-                className="text-sm md:text- lg:text-3xl tracking-[0.8rem] lowercase font-extralight leading-4"
+                className="text-2xl tracking-[0.8rem] lowercase font-extralight leading-4"
                 style={{ fontFamily: `${fonts.ct}` }}
               >
                 exchange
