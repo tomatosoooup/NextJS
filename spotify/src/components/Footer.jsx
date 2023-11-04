1c6e32dffc06f6f08a9f0d837e42ab5489de7a15
Vasyl <v.gabinet02@icloud.com>
2023-11-04 18:39:54 +0200

    added new sections, getting playlists

---
+++ spotify/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Footer = () => {
+  return <Container>Footer</Container>;
+};
+
+export default Footer;
+
+const Container = styled.div`
+  background-color: #181818;
+  height: 100%;
+`;
---
+++ spotify/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
