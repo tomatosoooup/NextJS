1c93055692648f2146c6de5533296b8206e76b3c
Vasyl <v.gabinet02@icloud.com>
2024-01-23 17:51:45 +0200

    Now adding db setup

--- next-auth-use/actions/register.ts
+++ next-auth-use/actions/register.ts
@@ -3,6 +3,10 @@
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
 
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
+
 // should mark use server to NOT bundle it with the client code
 // Equivalent of and API route
 
@@ -13,5 +17,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: Send verification token email
+
+  return { success: "User created!" };
 };
